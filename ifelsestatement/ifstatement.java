8f067df7ee6ab1eb36d8da7a65343f6741c6ef02
PawasUpreti <39916378+PawasUpreti@users.noreply.github.com>
2019-01-23 19:41:42 +0530

    Add files via upload

---
+++ ifelsestatement/ifstatement.java
@@ -0,0 +1,13 @@
+package ifstatement;
+
+public class ifstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 7;
+if(x<42) {
+	System.out.println("Hi");
+}
+	}
+
+}
